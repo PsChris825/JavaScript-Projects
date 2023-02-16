300ec09e36a315003ee59ad432ceff3ed79ac502
Chris <pillotchristopher@gmail.com>
2023-02-15 21:21:33 -0800

    created function project

---
+++ JavaScript Project/project2_functions/JS/main_js.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var sentence = "I am Learning"; //first variable
+    sentence += " a lot from this book!"; //added to the above using operators to concatenate the strings
+    document.getElementById("Concatenate").innerHTML = sentence; 
+}
+
