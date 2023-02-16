c1bfbb46dff912e904e82782f03e00c7a8706155
Chris <pillotchristopher@gmail.com>
2023-02-15 20:51:52 -0800

    created basic JS projects
    added variables and alert message

---
+++ JavaScript Project/Project1_expression_alert/main.js
@@ -0,0 +1,4 @@
+var x="This is the beginning of the string"//This is the first variable
+var y=" and this is the end of the string."//this is the second variable
+var z= x+y //This Concatenates variable 1(x) and variable 2(y)
+window.alert(z)//This is the window alert that includes both variations
